42efcf4a23ed8d8b44fbba884c1a2baaed0fe5f9
Sant <ignatius.varrel@gmail.com>
2025-02-23 15:42:15 +0700

    feat: add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Skills from "./components/Skills"
 import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 export default function App() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function App() {
       <Skills/>
       <Portfolio/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
