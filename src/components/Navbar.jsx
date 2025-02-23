4ef1e50a1a3889fac6365ffbd951e2f3e1609817
Sant <ignatius.varrel@gmail.com>
2025-02-23 12:53:25 +0700

    change Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,10 +32,10 @@ const Navbar = () => {
 
     return (
         <div className='fixed top-0 left-0 w-full bg-opacity-70 backdrop-blur-md z-50'>
-            <div className="max-w-[1300px] mx-auto bg-green-200 flex justify-between text-gray-200 text-xl items-center px-12 h-20">
-                <a href='#' className='text-black'>Ignatius Varrel Santoso</a>
+            <div className="max-w-[1300px] mx-auto flex justify-between text-gray-200 text-xl items-center px-12 h-20">
+                <a href='#'>Ignatius Varrel Santoso</a>
 
-                <ul className='hidden md:flex gap-12 z-10 cursor-pointer text-black'>
+                <ul className='hidden md:flex gap-12 z-10 cursor-pointer'>
                     <li>
                         <Link to="skills" smooth={true} offset={50} duration={500}>About</Link>
                     </li>
@@ -47,7 +47,7 @@ const Navbar = () => {
                     </li>
                 </ul>
 
-                <div onClick={toggleNav} className='md:hidden z-50 text-black'>
+                <div onClick={toggleNav} className='md:hidden z-50 text-gray-200'>
                     {nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30}/>}
                 </div>
 
@@ -55,7 +55,7 @@ const Navbar = () => {
                     initial={false}
                     animate={nav ? "open" : "closed"}
                     variants={menuVariants}
-                    className='fixed left-0 top-0 w-full min-h-screen bg-gray-700 z-40'
+                    className='fixed left-0 top-0 w-full min-h-screen bg-gray-900 z-40'
                 >
                     <ul className='font-semibold text-4xl space-y-8 mt-24 text-center'>
                         <li>
