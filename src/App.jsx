82899ad9ae157d27cb59bd0dfdcccbd8c358185b
Sant <ignatius.varrel@gmail.com>
2025-02-23 14:36:32 +0700

    feat: add Portfolio component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Skills from "./components/Skills"
+import Portfolio from "./components/Portfolio"
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero/>
       <Skills/>
+      <Portfolio/>
     </>
   )
 }
