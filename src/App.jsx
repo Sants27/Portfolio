9b12eba932be58f6c23b9a26511934ebcb5f41b0
Sant <ignatius.varrel@gmail.com>
2025-02-23 14:55:20 +0700

    feat: add Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Skills from "./components/Skills"
 import Portfolio from "./components/Portfolio"
+import Contact from "./components/Contact"
 export default function App() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function App() {
       <Hero/>
       <Skills/>
       <Portfolio/>
+      <Contact/>
     </>
   )
 }
