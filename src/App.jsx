d597a78609bc820bb41fbff79569211f7252df4c
Sant <ignatius.varrel@gmail.com>
2025-02-27 08:37:10 +0700

    feat: implement routing in App.jsx for multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Skills from "./components/Skills"
-import Portfolio from "./components/Portfolio"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+// import Navbar from "./components/Navbar"
+// import Hero from "./components/Hero"
+// import Skills from "./components/Skills"
+// import Portfolio from "./components/Portfolio"
+// import Contact from "./components/Contact"
+// import Footer from "./components/Footer"
+// export default function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Hero/>
+//       <Skills/>
+//       <Portfolio/>
+//       <Contact/>
+//       <Footer/>
+//     </>
+//   )
+// }
+
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <Hero/>
-      <Skills/>
-      <Portfolio/>
-      <Contact/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
