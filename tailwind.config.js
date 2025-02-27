c59e25bc3328ccbc2a697d961f1175855060b997
Sant <ignatius.varrel@gmail.com>
2025-02-27 08:45:56 +0700

    feat: add scrollPadding for navbar in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      scrollPadding: {
+        'nav': '6rem', // Sesuaikan dengan tinggi navbar
+      },
+    },
   },
   plugins: [],
 }
